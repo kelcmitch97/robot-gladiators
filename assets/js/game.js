0fbfc1cbbdc0a23832cf3b10e1880084a87d0915
kelcmitch97 <kelciemitchelmore@gmail.com>
2021-12-30 19:58:05 -0330

    add playerName to game.js file

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+// this creates a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("Our robot's name is " + playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
