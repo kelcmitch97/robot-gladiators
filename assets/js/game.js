9054b387a68069eecb21a6767409d1ada54f29a8
kelcmitch97 <kelciemitchelmore@gmail.com>
2021-12-31 11:43:54 -0330

    add player health and enemy health checks

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,48 @@
 // this creates a function named "fight"
 
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
 
-console.log(playerName);
+// check to see if the value of the playerHealth var is greater than 0
+if (playerHealth > 0) {
+    console.log("Your player is still alive!");
+}
+var playerAttack = 10;
 
-console.log("Our robot's name is " + playerName);
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-function fight() {
-    window.alert("The fight has begun!");
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators");
+
+    // subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' var
+    enemyHealth = enemyHealth - playerAttack;
+
+    // log a resulting message to the console so we know that it worked
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. ");
+
+    // check enemys health
+    if (enemyHealth <=0) {
+        window.alert(enemyName + " has died! ");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left. ");
+    }
+
+    // subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' var
+    playerHealth = playerHealth - enemyAttack;
+
+    // log a resulting message to the console so we know that it worked
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. ");
+
+    // check enemys health
+    if (playerHealth <=0) {
+        window.alert(playerName + " has died! ");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left. ");
+    }
 }
 
-// fight();
+fight();
